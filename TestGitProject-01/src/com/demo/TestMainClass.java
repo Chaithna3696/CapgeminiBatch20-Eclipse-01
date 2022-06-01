40cbb2e31e8ab924556143c105e2b6423e08c9aa
Chaithna3696 <chaithnareddy@gmail.com>
2022-06-01 17:01:11 +0530

    Second commit

--- TestGitProject-01/src/com/demo/TestMainClass.java
+++ TestGitProject-01/src/com/demo/TestMainClass.java
@@ -3,6 +3,8 @@ package com.demo;
 public class TestMainClass {
 	public static void main(String[] args) {
 		System.out.println("My first Git Hub project");
+		System.out.println("This is my second commit");
+
 	}
 
 }
